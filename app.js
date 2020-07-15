4751c36b86627456ab9364adabb427e7c3760a38
haniket <haniket2812001@gamil.com git config --global user.email haniket2812001@gamil.com>
2020-07-15 21:02:12 +0530

    safety-page

--- app.js
+++ app.js
@@ -9,24 +9,52 @@ app.get("/",function(req,res){
 res.render("home");
 
 });
+
+
+
+
 app.get("/books",function(req,res){
   res.render("books");
 });
+
+
+
 app.get("/faq",function(req,res){
   res.render("faq");
 });
+
+
+
 app.get("/addtocart",function(req,res){
   res.render("addtocart");
 });
+
+
+
 app.get("/courses",function(req,res){
   res.render("courses");
 });
+
+
+
+
 app.get("/detail",function(req,res){
   res.render("detail");
 });
+
+
+
 app.get("/signup",function(req,res){
   res.render("signup");
 });
+
+
+
+app.get("/safety",function(req,res){
+  res.render("safety");
+});
+
+
 app.listen(3000,function(){
  console.log("server is started on port 3000");
 
